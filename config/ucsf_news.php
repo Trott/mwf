7bd0d6c9e7fe39491475db7f105f09ea7ad9418c
Rich Trott <rtrott@gmail.com>
2011-07-13 17:59:50 -0700

    .htaccess file, large number of other files I missed in the last check-in, whoops.

---
+++ config/ucsf_news.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Configuration file for UCSF news module
+ *
+ * This should NOT be included directly; instead /assets/config.php should be.
+ *
+ * @author rtrott
+ * @version 20110310
+ *
+ * @uses Config
+ * @link /assets/config.php
+ */
+
+require_once(dirname(dirname(__FILE__)).'/lib/config.class.php');
+
+/**
+ * feeds
+ *
+ * Feeds to read in the news module.
+ * 
+ * The keys are codes to get a particular feed.  Pass as value for "feed" in the URL to get just that feed.
+ * 
+ * The values are:
+ *
+ *  - name :: the name that appears for the feed
+ *
+ *  - url :: the URL of the feed
+ *  
+ *  - date_format :: format string for date field; omit for a default
+ *  
+ *  - allowed_tags :: array of tags that should not be removed before displaying an item <description> from the RSS feed; omit for a default
+ *  
+ *  - direct_link :: boolean; when displaying the items in the feed, link to the <link> contents and not to our view of the <description> contents; omit for default of false
+ *
+ *  - header_title :: string; blank for default ('News')
+ *  
+ * @link /news/index.php
+ * @link /news/view.php
+ */
+Config::set('ucsf_news', 'feeds', array(
+				'ucsfnews'=>array('name'=>'UCSF News','url'=>'http://feeds.feedburner.com/UCSF_News', 'allowed_tags'=>array('b', 'i', 'p', 'a', 'em', 'strong')),
+				'mediacoverage'=>array('name'=>'Media Coverage','url'=>'http://feeds.feedburner.com/UCSF_Media_Coverage','allowed_tags'=>array('b', 'i', 'p', 'a', 'em', 'strong'),'direct_link'=>true)));
+
+
+/**
+ * alternate_feeds
+ *
+ * Feeds that can be specified in the "feed" URL parameter but will not appear on the default page.
+ * 
+ * The keys are codes to get a particular feed.  Pass as value for "feed" in the URL to get that feed.
+ * 
+ * The values are:
+ *
+ *  - name :: the name that appears for the feed
+ *
+ *  - url :: the URL of the feed
+ *  
+ *  - date_format :: format string for date field; omit for a default
+ *  
+ *  - allowed_tags :: array of tags that should not be removed before displaying an item <description> from the RSS feed; omit for a default
+ *  
+ *  - direct_link :: boolean; when displaying the items in the feed, link to the <link> contents and not to our view of the <description> contents; omit for default of false
+ *  
+ *  - header_title :: string; blank for default ('News')
+ *  
+ *  - hidden :: boolean; do not show feed in list of feeds on main page; omit for default of false
+ *  
+ * @link /news/index.php
+ * @link news/view.php
+ */
+Config::set('ucsf_news', 'alternate_feeds', array(
+				'facstaff'=>array('name'=>'UCSF Faculty/Staff News','url'=>'http://feeds.feedburner.com/UCSF_Faculty-Staff_News?format=xml','allowed_tags'=>array('b', 'i', 'p', 'a', 'em', 'strong')),
+				'ucsfevents'=>array('name'=>'UCSF Events','url'=>'http://feeds2.feedburner.com/ucsf/event-calendar','date_format'=>'l, F j, g:i A','header_title'=>'Events','hidden'=>true),
+				'pharmacy'=>array('name'=>'School of Pharmacy News','url'=>'http://pharmacy.ucsf.edu/news/20.xml'),
+				'library'=>array('name'=>'Library News','url'=>'http://www.library.ucsf.edu/news/all/feed')));
---
+++ root/emergency/index.php
@@ -0,0 +1,26 @@
+<?php
+require_once(dirname(__FILE__).'/../assets/config.php');
+require_once(dirname(__FILE__).'/../assets/lib/user_agent.class.php');
+require_once(dirname(__FILE__).'/../assets/lib/decorator.class.php');
+
+echo HTML_Decorator::html_start()->render();
+echo Site_Decorator::head()->set_title(Config::get('global', 'title_text') . ' | Emergency')->render();
+echo HTML_Decorator::body_start()->render();
+echo Site_Decorator::ucsf_header('Emergency');
+
+?>
+<div class="menu-full menu-detailed menu-padded">
+<ol>
+<li class="menu-first"><a href="tel:+911">911 <span class="smallprint">for emergencies only</span></a></li> 
+<li><a href="tel:+14154761414">UCSF Police Non-Emergency<br/><span class="smallprint">(415) 476-1414</span></a></li> 
+<li><a href="tel:+14158857890">UCSF Medical Center Security<br/><span class="smallprint">(415) 885-7890</span></a></li> 
+<li><a href="tel:+14155024000">Campus Emergency Information Hotline<br/><span class="smallprint">(415) 502-4000</span></a></li> 
+<li><a href="tel:+18008738232">Back-up Campus Emergency Hotline<br/><span class="smallprint">1 (800) 873-8232</span></a></li> 
+<li class="menu-last"><a href="tel:+14158857828">UCSF Medical Center Information Hotline<br/><span class="smallprint">(415) 885-7828</span></a></li> 
+</ol>
+</div>
+<a href="javascript:history.back()" id="button-top" class="button-full button-padded">Back</a>    
+<?php echo Site_Decorator::ucsf_footer()->render(); ?>
+
+</body>
+</html>

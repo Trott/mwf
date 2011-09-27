461d24fd6cd448213c3d34b1be5cc8566b752216
Rich Trott <rtrott@gmail.com>
2011-09-27 15:37:37 -0700

    new info per Michele M.

--- root/library/whereisit/studyspace/index.php
+++ root/library/whereisit/studyspace/index.php
@@ -11,21 +11,35 @@ echo Site_Decorator::ucsf_header('<a href="/library">Library</a>')
 <h1 class="content-first">Study Spaces</h1>
 
 <div>
-<h4>Parnassus Group Study Spaces</h4>
+<h4>Parnassus</h4>
+<h4><small>Parnassus Group Study Spaces</small></h4>
 <ul class="bulleted">
 <li>Available to UCSF students for group study during library hours</li>
 <li>Include WiFi, electrical outlet, whiteboard, X-ray light box</li>
-<li>Large monitors available in some rooms</li>
-<li>Check out at Service Desk</li>
-<li>UCSF borrowing account is required</li>
-<li>Advance reservations not available</li>
+<li>Check out at Service Desk, advance reservations not available</li>
 <li class="content-last">More info: <a href="tel:+14154762336">(415) 476-2336</a></li>
 </ul>
-</div>
 
+<h4><small>Faculty Carrels</small></h4>
+<ul class="bulleted">
+<li>Available to UCSF faculty during Parnassus library hours</li>
+<li>Daily, weekly, and monthly carrels available</li>
+<li class="content-last">Reservations and more info: <a href="tel:+14154762336">(415) 476-2336</a></li>
+</ul>
+</div>
 <div>
-<h4>Other Study Spaces</h4>
-<p>Enter with proxy/ID card</p>
+<h4>Mission Bay</h4>
+<ul class="bulleted">
+<li>FAMRI Library</li>
+<li>Library computer lab CC151: UCSF student ID access whenever the Rutter Center is open</li>
+<li>Genentech Hall Room N-114: Designated open study hall from 9pm - 6am</li>
+<li class="content-last">Genentech Hall Classrooms: Available when classes are not in session. S-201, S-202, S-204, S-261, S-271</li>
+
+</ul>
+</div>
+<div>
+
+<h4>ID Card Required</h4>
 <ul class="bulleted">
 <li><a href="/library/whereisit/computers">Computer Spaces</a></li>
 <li>Hearst Room, Parnassus Library:<br />
@@ -34,16 +48,6 @@ echo Site_Decorator::ucsf_header('<a href="/library">Library</a>')
 </ul>
 </div>
 
-<div>
-<h4>Faculty Carrels</h4>
-<ul class="bulleted">
-<li>Available to UCSF faculty during Parnassus library hours</li>
-<li>Daily, weekly, and monthly carrels available</li>
-<li>Include WiFi and electrical outlets</li>
-<li>UCSF borrowing account is required</li>
-<li class="content-last">Reservations and more info: <a href="tel:+14154762336">(415) 476-2336</a></li>
-</ul>
-</div>
 </div>
 
 <a href="javascript:history.back()" id="button-top" class="button-full button-padded">Back</a> 

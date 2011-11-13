374fac9375093f92e574de165743bc4073b8e402
Rich Trott <rtrott@gmail.com>
2011-11-13 14:09:19 -0800

    Cache emergency numbers page

--- root/emergency.php
+++ root/emergency.php
@@ -3,7 +3,7 @@ require_once(dirname(__FILE__).'/assets/config.php');
 require_once(dirname(__FILE__).'/assets/lib/user_agent.class.php');
 require_once(dirname(__FILE__).'/assets/lib/decorator.class.php');
 
-echo HTML_Decorator::html_start()->render();
+echo HTML_Decorator::html_start(array('manifest' => '/manifest.appcache'))->render();
 echo Site_Decorator::head()->set_title(Config::get('global', 'title_text') . ' | Emergency')->render();
 echo HTML_Decorator::body_start()->render();
 echo Site_Decorator::ucsf_header('<a href="/emergency">Emergency</a>')->render();

cc2552c633e39c68532f4a5aa8c3530f17618ba1
Rich Trott <rtrott@gmail.com>
2011-11-15 06:31:54 -0800

    Enable orientation changes.  Still have to make them look pretty...

--- auxiliary/native/ios/UCSFMobile/MWFWebView.m
+++ auxiliary/native/ios/UCSFMobile/MWFWebView.m
@@ -99,7 +99,9 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return (interfaceOrientation == UIInterfaceOrientationPortrait || 
+			interfaceOrientation == UIInterfaceOrientationLandscapeLeft || 
+			interfaceOrientation == UIInterfaceOrientationLandscapeRight);
 }
 
 

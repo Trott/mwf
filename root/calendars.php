b08359b3ea5a1fa93f189274b5a9a97a77f4dec8
Rich Trott <rtrott@gmail.com>
2011-11-18 22:54:25 -0800

    Appcache for calendars

--- root/calendars.php
+++ root/calendars.php
@@ -1,7 +1,7 @@
 <?php
-require_once(dirname(dirname(__FILE__)) . '/assets/lib/decorator.class.php');
-require_once(dirname(dirname(__FILE__)) . '/assets/config.php');
-echo HTML_Decorator::html_start()->render();
+require_once(dirname(__FILE__) . '/assets/lib/decorator.class.php');
+require_once(dirname(__FILE__) . '/assets/config.php');
+echo HTML_Decorator::html_start(array('manifest' => '/assets/appcache.php'))->render();
 echo Site_Decorator::head()->set_title(Config::get('global', 'title_text') . " | Calendars")->render();
 echo HTML_Decorator::body_start()->render();
 echo Site_Decorator::ucsf_header('<a href="/calendars">Calendars</a>')

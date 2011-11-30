131589ad9734e42a88debb8624272d3d0b99fa1f
Rich Trott <rtrott@gmail.com>
2011-11-30 13:57:16 -0800

    Changes per Michele M., more aggressive caching

--- root/library/index.php
+++ root/library/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(dirname(__FILE__)).'/assets/lib/decorator.class.php');
 require_once(dirname(dirname(__FILE__)).'/assets/config.php');
-echo HTML_Decorator::html_start(array('manifest' => '/assets/appcache.php'))->render();
+echo HTML_Decorator::html_start()->render();
 echo Site_Decorator::head()->set_title(Config::get('global', 'title_text') . " | Library")->render();
 echo HTML_Decorator::body_start()->render();
 echo Site_Decorator::ucsf_header('<a href="/library">Library</a>')
@@ -11,14 +11,11 @@ echo Site_Decorator::ucsf_header('<a href="/library">Library</a>')
  
          <ol> 
             <li class="menu-first"><a href="locations">Hours and Locations</a></li>
-            <li><a href="http://ucsf.worldcat.org/m"><span class="external">Find Books/Journals</span></a></li>
-            <li><a href="askus">Ask Us</a></li>
+            <li><a href="http://ucsf.worldcat.org/m"><span class="external">Find Books and Journals</span></a></li>
             <li><a href="http://guides.library.ucsf.edu/content.php?pid=252446&sid=2084303"><span class="external">Mobile Resources</span></a></li>
-            <li><a href="http://guides.library.ucsf.edu/index.php"><span class="external">Getting Started Guides</span></a></li>
-            <li><a href="howdoi">How Do I?</a></li>
-            <li><a href="whereisit">Where Is It?</a></li>
+            <li><a href="help/">Get Help</li></a>
             <li><a href="/news/library">News</a></li>
-            <li class="menu-last"><a href="http://library.ucsf.edu/?ovrrdr=1"><span class="external">Library Website</span></a></li>
+            <li class="menu-last"><a href="http://library.ucsf.edu/?ovrrdr=1"><span class="external">Full Library Site</span></a></li>
          </ol> 
     </div> 
 <?php 

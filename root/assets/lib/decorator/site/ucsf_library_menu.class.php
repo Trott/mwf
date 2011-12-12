b734725dc99c32f640d5fb3a4760f89c8314f7f0
Rich Trott <rtrott@gmail.com>
2011-12-12 10:53:31 -0800

    Library News link fix

--- root/assets/lib/decorator/site/ucsf_library_menu.class.php
+++ root/assets/lib/decorator/site/ucsf_library_menu.class.php
@@ -26,7 +26,7 @@ class Ucsf_Library_Menu_Site_Decorator extends Menu_Full_Site_Decorator {
         $this->add_item('<span class="external">Find Books and Journals</span>', 'http://ucsf.worldcat.org/m', array());
         $this->add_item('<span class="external">Mobile Resources</span>', 'http://guides.library.ucsf.edu/content.php?pid=252446&sid=2084303', array());
         $this->add_item('Get Help', '/library/help/', array());
-        $this->add_item('News', '/news/library/', array());
+        $this->add_item('News', '/news/library', array());
         $this->add_item('<span class="external">Full Library Site</span>','http://library.ucsf.edu/?ovrrdr=1',array());
         return parent::render();
     }

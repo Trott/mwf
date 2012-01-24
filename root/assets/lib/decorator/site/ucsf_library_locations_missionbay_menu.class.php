fdd9af5d67fd665962efca179ed7843b3fe86132
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2012-01-24 18:32:34 -0500

    refactored location menu decorator out into its own parent class for better reusability. modified the parnassus and mission-bay location decorators to inherit from the new base class.

--- root/assets/lib/decorator/site/ucsf_library_locations_missionbay_menu.class.php
+++ root/assets/lib/decorator/site/ucsf_library_locations_missionbay_menu.class.php
@@ -6,25 +6,24 @@
  * @subpackage site_decorator
  *
  * @author trott
+ * @author topfstedt
  * @copyright Copyright (c) 2010-11 UC Regents
  * @license http://mwf.ucla.edu/license
- * @version 20111207
+ * @version 20120124
  *
- * @uses Menu_Full_Site_Decorator
+ * @uses Ucsf_Library_Location_Menu_Site_Decorator
  */
-require_once(dirname(__FILE__) . '/menu_full.class.php');
+require_once(dirname(__FILE__) . '/ucsf_library_location_menu.class.php');
 
-class Ucsf_Library_Locations_Missionbay_Menu_Site_Decorator extends Menu_Full_Site_Decorator {
+class Ucsf_Library_Locations_Missionbay_Menu_Site_Decorator extends Ucsf_Library_Location_Menu_Site_Decorator {
 
-    public function __construct($title = 'Mission Bay Library', $params = array()) {
-        parent::__construct($title, $params);
+	public function __construct($title = 'Mission Bay Library', $params = array()) {
+        parent::__construct($title, $params,
+        		'/maps/map.php?loc=Rutter+Center',
+        		"William J. Rutter Conference Center\nRoom 150\n1675 Owens Street\nSan Francisco, CA 94143-2119",
+        		'(415) 514-4060',
+        		"Mon - Thurs: 9:00 am - 9:00 pm\nFri: 9:00 am - 5:30 pm\nSat - Sun: CLOSED",
+        		'http://library.ucsf.edu/locations/hours?ovrrdr=1');
         $this->set_padded()->set_detailed();
-    }
-
-    public function render() {
-        $this->add_item('Map<br/><br/><span class="smallprint">William J. Rutter Conference Center<br/>Room 150<br/>1675 Owens Street<br/>San Francisco, CA 94143-2119</span>', '/maps/map.php?loc=Rutter+Center', array());
-        $this->add_item('(415)&nbsp;514-4060', 'tel:+14155144060', array());
-        $this->add_item('<span class="smallprint">Mon - Thurs: 9:00 am - 9:00 pm<br/>Fri: 9:00 am - 5:30 pm<br/>Sat - Sun: CLOSED</span><br/><br/><span class="external">Holidays and exceptions</span>', 'http://library.ucsf.edu/locations/hours?ovrrdr=1', array(),array('rel'=>'external','class'=>'no-ext-ind'));
-        return parent::render();
     }
 }

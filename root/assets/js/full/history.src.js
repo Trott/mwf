21a0a27906dd48e68ff3d39641f7d8a7359c1d67
Rich Trott <rtrott@gmail.com>
2012-02-01 22:50:27 -0800

    For weirdness in Android native app, small hack that won't be necessary after layout updates slated for next week

--- root/assets/js/full/history.src.js
+++ root/assets/js/full/history.src.js
@@ -13,6 +13,7 @@
  * @requires mwf
  * @requires mwf.standard.preferences
  * @requires mwf.full.fastLink
+ * @requires mwf.userAgent
  * 
  */
 
@@ -59,8 +60,10 @@ mwf.full.history = new function() {
                     hideElement.setAttribute("style","display:none");
             }
             var showElement = document.getElementById(show);
+            var displayValue = (mwf.userAgent.isNative() && mwf.userAgent.getOS()=='android' && show=="main_menu") ?
+                "display:inline" : "display:block";
             if (showElement)
-                showElement.setAttribute("style","display:block");
+                showElement.setAttribute("style",displayValue);
             else
                 return false;
             var topButton = document.getElementById('button-top');

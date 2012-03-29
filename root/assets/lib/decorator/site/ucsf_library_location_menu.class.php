9d457ffc1896e6020afc2d70e8452e5496359bef
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2012-03-29 19:55:04 -0400

    fixed up render function to allow for raw markup content output everywhere (if specified)

--- root/assets/lib/decorator/site/ucsf_library_location_menu.class.php
+++ root/assets/lib/decorator/site/ucsf_library_location_menu.class.php
@@ -32,7 +32,7 @@ class Ucsf_Library_Location_Menu_Site_Decorator extends Menu_Site_Decorator {
         $this->set_padded()->set_detailed();
     }
 
-    public function render() {
+    public function render($raw = false) {
         $address_array = preg_split("/[\n]+/", $this->_address);
         $address = array();
         foreach ($address_array as $address_element) {
@@ -54,7 +54,7 @@ class Ucsf_Library_Location_Menu_Site_Decorator extends Menu_Site_Decorator {
         $this->add_item(array(HTML_Decorator::tag('span', $hours, array('class' => 'smallprint')),
             HTML_Decorator::tag_open('br'), HTML_Decorator::tag_open('br'),
             HTML_Decorator::tag('span', 'Holidays and exceptions', array('class'=>'external'))), $this->_extLink, array(), array('rel' => 'external', 'class' => 'no-ext-ind'));
-        return parent::render();
+        return parent::render($raw);
     }
 
 }
--- root/assets/lib/decorator/site/ucsf_library_menu.class.php
+++ root/assets/lib/decorator/site/ucsf_library_menu.class.php
@@ -21,7 +21,7 @@ class Ucsf_Library_Menu_Site_Decorator extends Menu_Site_Decorator {
         $this->set_padded()->set_detailed();
     }
 
-    public function render() {
+    public function render($raw = false) {
         $this->add_item('Hours and Locations', '/library/locations', array());
         $this->add_item('Find Books and Journals', 'http://ucsf.worldcat.org/m', array(), array('rel'=>'external'));
         $this->add_item('Mobile Resources', 'http://guides.library.ucsf.edu/content.php?pid=252446&sid=2084303', array(), array('rel'=>'external'));
@@ -29,7 +29,7 @@ class Ucsf_Library_Menu_Site_Decorator extends Menu_Site_Decorator {
         $this->add_item('Get Help', '/library/help/', array());
         $this->add_item('News', '/news/library', array());
         $this->add_item('Full Library Site','http://library.ucsf.edu/?ovrrdr=1',array(), array('rel'=>'external'));
-        return parent::render();
+        return parent::render($raw);
     }
 
 }

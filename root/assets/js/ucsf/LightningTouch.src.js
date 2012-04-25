4861b92573b53e4a54df852b25f623c70ff87911
Rich Trott <rtrott@gmail.com>
2012-04-25 01:29:56 -0700

    comment formatting

--- root/assets/js/ucsf/LightningTouch.src.js
+++ root/assets/js/ucsf/LightningTouch.src.js
@@ -2,7 +2,7 @@
  * Lightning Touch
  * 
  * Make switching divs in response to touched links responsive without the several
- *    hundred millisecond delay typical in a hendheld touchscreen browser.
+ * hundred millisecond delay typical in a hendheld touchscreen browser.
  *
  * @author Richard Trott
  * @copyright Copyright (c) 2012 UC Regents

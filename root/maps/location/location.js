263cd4c8e59abc3d19db273590b1e2011d4c178e
Rich Trott <rtrott@gmail.com>
2012-05-27 19:20:21 -0700

    Apparently we don't need location.obj.js

--- root/maps/location/location.js
+++ root/maps/location/location.js
@@ -1,5 +1,2 @@
 if ((typeof google == 'undefined') || (typeof google.maps == 'undefined'))
     mwf.util.importJS('http://maps.google.com/maps/api/js?sensor=true');
-
-mwf.util.importJS(mwf.site.webroot()+'/maps/location/location.obj.js');
-

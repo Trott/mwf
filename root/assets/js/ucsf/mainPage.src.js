7e4312944fb9ef3d85a712b59d48854232257547
Rich Trott <rtrott@gmail.com>
2012-06-04 11:09:04 -0700

    JSLint for UCSF JS

--- root/assets/js/ucsf/mainPage.src.js
+++ root/assets/js/ucsf/mainPage.src.js
@@ -1,20 +1,22 @@
-ucsf = ucsf || {};
+var ucsf = ucsf || {};
 
 ucsf.callAnalytics = function () {
-    var path = (window.location.hash == '#/main_menu') ? '/' : window.location.hash.substr(4);
+    "use strict";
+    var path = (window.location.hash === '#/main_menu') ? '/' : window.location.hash.substr(4);
     mwf.analytics.trackPageview(path);
-}
+};
 
 window.addEventListener('hashchange', ucsf.callAnalytics, false);
 
 ucsf.toggleHeaderAndFooter = function () {
-    var header = document.getElementById('header');
-            
-    var headFootStyle = location.hash=="#/main_menu" ? "" : "display:block";
-            
-    if (header)
-        header.setAttribute("style",headFootStyle);
-}
+    "use strict";
+    var header = document.getElementById('header'),
+        headFootStyle = location.hash === "#/main_menu" ? "" : "display:block";
+
+    if (header) {
+        header.setAttribute("style", headFootStyle);
+    }
+};
 
 window.addEventListener('hashchange', ucsf.toggleHeaderAndFooter, false);
 window.addEventListener('DOMContentLoaded', ucsf.toggleHeaderAndFooter, false);

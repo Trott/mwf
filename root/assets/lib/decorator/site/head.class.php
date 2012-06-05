48979a2753d1ff017cf486c524e37b7fadf3f60c
Rich Trott <rtrott@gmail.com>
2012-06-04 21:48:57 -0700

    Specify charset, rm unneeded external libs

--- root/assets/lib/decorator/site/head.class.php
+++ root/assets/lib/decorator/site/head.class.php
@@ -13,12 +13,9 @@
  *
  * @uses Decorator
  * @uses Tag_HTML_Decorator
- * @uses Config
  */
 
 require_once(dirname(dirname(__DIR__)).'/decorator.class.php');
-require_once(dirname(dirname(__DIR__)).'/config.class.php');
-require_once(dirname(dirname(__DIR__)).'/https.class.php');
 require_once(dirname(__DIR__).'/html/tag.class.php');
 
 class Head_Site_Decorator extends Tag_HTML_Decorator
@@ -45,24 +42,21 @@ class Head_Site_Decorator extends Tag_HTML_Decorator
     
     public function render($raw=false)
     {   
-        $handler_css = Config::get('global', 'site_assets_url').'/css/main.css';
+        $handler_css = '/assets/css/main.css';
 
-        $handler_js = Config::get('global', 'site_assets_url').'/js.php';
+        $handler_js = '/assets/js.php';
         
         $this->add_inner_tag_front('meta', false, array('name'=>'viewport', 'content'=>'width=device-width,initial-scale=1,maximum-scale=1'));
         $this->add_inner_tag_front('script', null, array('async'=>'', 'src'=>'//www.google-analytics.com/ga.js'));
-        $this->add_inner_tag_front('script', null, array('async'=>'', 'src'=>(HTTPS::is_https() ? HTTPS::convert_path($handler_js) : $handler_js)));
+        $this->add_inner_tag_front('script', null, array('async'=>'', 'src'=>$handler_js));
         
         $this->add_inner_tag_front('link', false, array('rel'=>'apple-touch-icon', 'href'=>'//assets/img/ucsf-appicon.png'));
         $this->add_inner_tag_front('link', false, array('rel'=>'apple-touch-icon-precomposed', 'href'=>'//assets/img/ucsf-appicon.png'));
         
-        $this->add_inner_tag_front('link', false, array('rel'=>'stylesheet', 'type'=>'text/css', 'href'=>(HTTPS::is_https() ? HTTPS::convert_path($handler_css) : $handler_css), 'media'=>'screen'));
+        $this->add_inner_tag_front('link', false, array('rel'=>'stylesheet', 'type'=>'text/css', 'href'=>$handler_css, 'media'=>'screen'));
         $this->add_inner_tag_front('title', $this->_title);
 
-        $charset = Config::get('global', 'charset');
-        if ($charset !== false) {
-            $this->add_inner_tag_front('meta', false, array('charset'=>$charset));
-        }
+        $this->add_inner_tag_front('meta', false, array('charset'=>'utf-8'));
         
         return parent::render($raw);
     }

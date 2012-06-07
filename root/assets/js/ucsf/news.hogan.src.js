56ff408a38cda72cb0447263723e8e93c3db8209
Rich Trott <rtrott@gmail.com>
2012-06-07 11:04:05 -0700

    Error message in template

--- root/assets/js/ucsf/news.hogan.src.js
+++ root/assets/js/ucsf/news.hogan.src.js
@@ -2,14 +2,22 @@
 var hogan = require("hogan.js");
 
 // construct template string
-var template = '<div class="menu detailed">';
+var template = '{{#feed}}'
+    template += '<div class="menu detailed">';
     template += '<h2 clas="light">{{title}}</h2>';
     template += '<ol>';
     template += '{{#entries}}<li><a rel="external" href="{{link}}">{{title}}</a></li>{{/entries}}'
     template += '</ol>';
     template += '</div>';
+    template += '{{/feed}}';
+    template += '{{^feed}}';
+    template += '<div class="content">';
+    template += '<p>News feed could not be loaded.</p>';
+    template += '</div>';
+    template += '{{/feed}}';
 
 // compile template
 var output = hogan.compile(template, {asString: true});
 
 console.log(output);
+
--- root/assets/js/ucsf/news.src.js
+++ root/assets/js/ucsf/news.src.js
@@ -2,25 +2,7 @@ ucsf.news = {
     headlines: function (container, storageId, feedUrl) {
         "use strict";
         // template precompiled via Hogan.JS from news.hogan.src.js
-        var template = new Hogan.Template(function(c,p,i){
-            var _=this;
-            _.b(i=i||"");
-            _.b("<div class=\"menu detailed\"><h2 clas=\"light\">");
-            _.b(_.v(_.f("title",c,p,0)));
-            _.b("</h2><ol>");
-            if(_.s(_.f("entries",c,p,1),c,p,0,74,130,"{{ }}")){
-                _.rs(c,p,function(c,p,_){
-                    _.b("<li><a rel=\"external\" href=\"");
-                    _.b(_.v(_.f("link",c,p,0)));
-                    _.b("\">");
-                    _.b(_.v(_.f("title",c,p,0)));
-                    _.b("</a></li>");
-                });
-                c.pop();
-            }
-            _.b("</ol></div>");
-            return _.fl();;
-        }),
+        var template = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");if(_.s(_.f("feed",c,p,1),c,p,0,9,162,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<div class=\"menu detailed\"><h2 clas=\"light\">");_.b(_.v(_.f("title",c,p,0)));_.b("</h2><ol>");if(_.s(_.f("entries",c,p,1),c,p,0,83,139,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<li><a rel=\"external\" href=\"");_.b(_.v(_.f("link",c,p,0)));_.b("\">");_.b(_.v(_.f("title",c,p,0)));_.b("</a></li>");});c.pop();}_.b("</ol></div>");});c.pop();}if(!_.s(_.f("feed",c,p,1),c,p,1,0,0,"")){_.b("<div class=\"content\"><p>News feed could not be loaded.</p></div>");};return _.fl();;}),
         feed;
         
         feed = new google.feeds.Feed(feedUrl);
@@ -28,7 +10,7 @@ ucsf.news = {
             var content = {},
             stored;
             if (! result.error) {
-                content = this.feed;
+                content = {"feed": this.feed};
                 if (Modernizr.localstorage) {
                     window.localStorage.setItem(storageId, JSON.stringify(content));
                 }
@@ -40,7 +22,6 @@ ucsf.news = {
                     }
                 }
             }
-            //TODO: add error message on empty object to template 
             container.innerHTML = template.render(content);
         });
     }
@@ -54,3 +35,5 @@ ucsf.news = {
 //TODO: Maybe in view, but "Loading..." should look better and should go away if there's an error.
 //TODO: google is undefined error when loaded without network
 //TODO: whoa, what's up with Modernizr not being defined in the shuttle planner?
+//TODO: get it to work on home page, at #/il/news, and at /news
+//TODO: maybe add some noscript value?

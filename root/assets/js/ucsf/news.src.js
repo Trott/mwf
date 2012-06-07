81703c688f743253f2d471213c55e41c32eacff9
Rich Trott <rtrott@gmail.com>
2012-06-06 23:46:19 -0700

    Add localstorage

--- root/assets/js/ucsf/news.src.js
+++ root/assets/js/ucsf/news.src.js
@@ -1,37 +1,55 @@
 ucsf.news = {
     headlines: function (container, storageId, feedUrl) {
         "use strict";
-        // template compiled via Hogan.JS from news.hogan.src.js
-        var template = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");_.b("<div class=\"menu detailed\"><h2 clas=\"light\">");_.b(_.v(_.f("title",c,p,0)));_.b("</h2><ol>");if(_.s(_.f("entries",c,p,1),c,p,0,74,130,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<li><a rel=\"external\" href=\"");_.b(_.v(_.f("link",c,p,0)));_.b("\">");_.b(_.v(_.f("title",c,p,0)));_.b("</a></li>");});c.pop();}_.b("</ol></div>");return _.fl();;}),
-            feed;
-
-        if (Modernizr.localstorage) {
-            feed = window.localStorage.getItem(storageId);
-            if (feed !== null) {
-        //TODO: check date on feed and discard if too old 
-        }
-        }
+        // template precompiled via Hogan.JS from news.hogan.src.js
+        var template = new Hogan.Template(function(c,p,i){
+            var _=this;
+            _.b(i=i||"");
+            _.b("<div class=\"menu detailed\"><h2 clas=\"light\">");
+            _.b(_.v(_.f("title",c,p,0)));
+            _.b("</h2><ol>");
+            if(_.s(_.f("entries",c,p,1),c,p,0,74,130,"{{ }}")){
+                _.rs(c,p,function(c,p,_){
+                    _.b("<li><a rel=\"external\" href=\"");
+                    _.b(_.v(_.f("link",c,p,0)));
+                    _.b("\">");
+                    _.b(_.v(_.f("title",c,p,0)));
+                    _.b("</a></li>");
+                });
+                c.pop();
+            }
+            _.b("</ol></div>");
+            return _.fl();;
+        }),
+        feed;
         
-        if (feed === null) {
-            feed = new google.feeds.Feed(feedUrl);
-            feed.load(function (result) {
-                if (! result.error) {
-                    container.innerHTML = template.render(this.feed);
-                } else {
-                    //TODO: sucks, make better
-                    alert(result.error.message);
+        feed = new google.feeds.Feed(feedUrl);
+        feed.load(function (result) {
+            var content = {},
+            stored;
+            if (! result.error) {
+                content = this.feed;
+                if (Modernizr.localstorage) {
+                    window.localStorage.setItem(storageId, JSON.stringify(content));
                 }
-            });
-        if (Modernizr.localstorage) {
-    //TODO: store the feed in localstorage
+            } else {
+                if (Modernizr.localstorage) {
+                    stored = window.localStorage.getItem(storageId);
+                    if (stored !== null) {
+                        content = JSON.parse(stored);
+                    }
+                }
+            }
+            //TODO: add error message on empty object to template 
+            container.innerHTML = template.render(content);
+        });
     }
-    //TODO: render if from localstorage--right now we only handle loading from the network
-    }
-}
 };
 
+//TODO: minimize this thing
 //TODO: add relevant stuff to offline appcache manifest. Is that going to even be possible? Does the Feed API require the network?
 //    Maybe don't load the feed API unless there's nothing in  localStroage? Urgh....
 //TODO: Maybe the thing to do is to try to load the Feed API/feeds with a low timeout and if that fails then use localstorage regardles
 //   of how out-of-date it may be?
 //TODO: Maybe in view, but "Loading..." should look better and should go away if there's an error.
+//TODO: google is undefined error when loaded without network

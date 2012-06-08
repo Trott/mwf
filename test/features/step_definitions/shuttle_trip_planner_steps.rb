72ba82dcc1b75f99cad9d211aea62072b87f0546
Rich Trott <rtrott@gmail.com>
2012-06-08 11:44:19 -0700

    Library News Hoganized

--- test/features/step_definitions/shuttle_trip_planner_steps.rb
+++ test/features/step_definitions/shuttle_trip_planner_steps.rb
@@ -8,11 +8,8 @@ Given /I visit the Shuttle Trip Planner page/ do
 end
 
 Then /^I should see the route "([^"]*)" "([^"]*)"$/ do |id, value|
-  #wait_until { find("#" + id + " option[selected]").text == value }
-  #assert_equal(value, find("#" + id + " option[selected]").text)
   select = find('#' + id)
   select.find(:xpath, XPath::HTML.option(value)).should be_selected
-#  find("#" + id + "
 end
 
 Then /^I select a route "([^"]*)" "([^"]*)"$/ do |id, value|

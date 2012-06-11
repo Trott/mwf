2bd86997adb23214c44fa46921a761e54ab5c272
Rich Trott <rtrott@gmail.com>
2012-06-11 13:54:59 -0700

    JS linting

--- root/assets/js/ucsf/news.hogan.src.js
+++ root/assets/js/ucsf/news.hogan.src.js
@@ -2,16 +2,16 @@
 var hogan = require("hogan.js");
 
 // construct template string
-var template = '{{#feed}}'
+var template = '{{#feed}}';
     template += '<div class="menu detailed">';
     template += '<h2>{{title}}</h2>';
     template += '<ol>';
     template += '{{#entries}}';
     template += '  <li>';
-    template += '    <a class="no-ext-ind" rel="external" href="{{link}}"><span class="external">{{title}}</span>'
-    template += '    <div class="smallprint light">{{dateTime.date}}</div>'
+    template += '    <a class="no-ext-ind" rel="external" href="{{link}}"><span class="external">{{title}}</span>';
+    template += '    <div class="smallprint light">{{dateTime.date}}</div>';
     template += '    <div class="smallprint light">{{dateTime.time}}</div></a>';
-    template += '{{/entries}}'
+    template += '{{/entries}}';
     template += '</ol>';
     template += '</div>';
     template += '{{/feed}}';
@@ -25,4 +25,3 @@ var template = '{{#feed}}'
 var output = hogan.compile(template, {asString: true});
 
 console.log(output);
-

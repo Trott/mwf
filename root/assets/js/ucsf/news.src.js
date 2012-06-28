a85c4921b84d88eba148c6fa4bf0baf231e95e03
Rich Trott <rtrott@gmail.com>
2012-06-28 16:21:52 -0700

    Make JSAPI async to avoid SPOF

--- root/assets/js/ucsf/news.src.js
+++ root/assets/js/ucsf/news.src.js
@@ -1,6 +1,7 @@
 /* globals ucsf:true, Hogan:true, google:true, Modernizr:true */
-ucsf.news = {
-    loadFromStorage: function(storageId) {
+ucsf.news = (function () {
+    
+    this.loadFromStorage = function (storageId) {
         var stored;
         if (Modernizr.localstorage) {
             stored = window.localStorage.getItem(storageId);
@@ -9,14 +10,9 @@ ucsf.news = {
             }
         }
         return {};
-    },
+    };
 
-    headlines: function (container, storageId, feedUrl, options) {
-        "use strict";
-        // template precompiled via Hogan.JS from news.hogan.src.js
-        var feed,
-        template;
-
+    this.render = function (container, storageId, feedUrl, options) {
         options = options || {};
 
         if (options.template) {
@@ -24,63 +20,82 @@ ucsf.news = {
         } else {
             template = new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");if(_.s(_.f("feed",c,p,1),c,p,0,9,313,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<div class=\"menu detailed\"><h2>");_.b(_.v(_.f("title",c,p,0)));_.b("</h2><ol>");if(_.s(_.f("entries",c,p,1),c,p,0,70,290,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("  <li>    <a class=\"no-ext-ind\" rel=\"external\" href=\"");_.b(_.v(_.f("link",c,p,0)));_.b("\"><span class=\"external\">");_.b(_.v(_.f("title",c,p,0)));_.b("</span>    <div class=\"smallprint light\">");_.b(_.v(_.d("dateTime.date",c,p,0)));_.b("</div>    <div class=\"smallprint light\">");_.b(_.v(_.d("dateTime.time",c,p,0)));_.b("</div></a>");});c.pop();}_.b("</ol></div>");});c.pop();}if(!_.s(_.f("feed",c,p,1),c,p,1,0,0,"")){_.b("<div class=\"content\"><p>News feed could not be loaded.</p></div>");};return _.fl();;});
         }
-        if (typeof google !== "undefined") {
 
-           feed = new google.feeds.Feed(feedUrl);
+        var feed,
+            template;
 
-            if (options.numEntries) {
-                feed.setNumEntries(options.numEntries);
-            }
+        feed = new google.feeds.Feed(feedUrl);
 
-            feed.load(function (result) {
-                var i,
-                    thisEntry,
-                    dateTime,
-                    dateObject,
-                    hours,
-                    minutes,
-                    designation,
-                    content = {};
+        if (options.numEntries) {
+            feed.setNumEntries(options.numEntries);
+        }
 
-                if (! result.error) {
-                    content = {
-                        "feed": this.feed
-                    };
-                    for (i = 0; i < content.feed.entries.length; i = i + 1) {
-                        thisEntry = content.feed.entries[i];
-                        dateTime = {};
-                        dateObject = new Date(thisEntry.publishedDate);
-                        dateTime.date = dateObject.toLocaleDateString();
-                        if (options.showTime) {
-                            minutes = dateObject.getMinutes();
-                            if (minutes < 10) {
-                                minutes = "0" + minutes;
-                            }
-                            hours = dateObject.getHours();
-                            designation = hours < 12 ? 'AM' : 'PM';
-                            if (hours > 12) {
-                                hours = hours - 12;
-                            }
-                            if (hours === 0) {
-                                hours = 12;
-                            }
-                            
-                            dateTime.time = hours + ':' +
-                                minutes + ' ' +
-                                designation;
+        feed.load(function (result) {
+            var i,
+            thisEntry,
+            dateTime,
+            dateObject,
+            hours,
+            minutes,
+            designation,
+            content = {};
+
+            if (! result.error) {
+                content = {
+                    "feed": this.feed
+                };
+                for (i = 0; i < content.feed.entries.length; i = i + 1) {
+                    thisEntry = content.feed.entries[i];
+                    dateTime = {};
+                    dateObject = new Date(thisEntry.publishedDate);
+                    dateTime.date = dateObject.toLocaleDateString();
+                    if (options.showTime) {
+                        minutes = dateObject.getMinutes();
+                        if (minutes < 10) {
+                            minutes = "0" + minutes;
                         }
-                        thisEntry.dateTime = dateTime;
+                        hours = dateObject.getHours();
+                        designation = hours < 12 ? 'AM' : 'PM';
+                        if (hours > 12) {
+                            hours = hours - 12;
+                        }
+                        if (hours === 0) {
+                            hours = 12;
+                        }
+
+                        dateTime.time = hours + ':' +
+                        minutes + ' ' +
+                        designation;
                     }
-                    if (Modernizr.localstorage) {
-                        window.localStorage.setItem(storageId, JSON.stringify(content));
-                    }
-                } else {
-                    content = ucsf.news.loadFromStorage(storageId);
+                    thisEntry.dateTime = dateTime;
                 }
-                container.innerHTML = template.render(content);
-            });
+                if (Modernizr.localstorage) {
+                    window.localStorage.setItem(storageId, JSON.stringify(content));
+                }
+            } else {
+                content = ucsf.news.loadFromStorage(storageId);
+            }
+            container.innerHTML = template.render(content);
+        });
+    };
+
+    this.headlines = function (container, storageId, feedUrl, options) {
+        "use strict";
+        
+        if (typeof google !== "undefined") {
+            if (typeof google.feeds == "undefined") {
+                google.load("feeds","1",{nocss:true, callback:function () { ucsf.news.render(container, storageId, feedUrl, options);}});
+            } else {
+                this.render(container, storageId, feedUrl, options);
+            }            
         } else {
             container.innerHTML = template.render(ucsf.news.loadFromStorage(storageId));
         }
-    }
-};
+    };
+
+    var script = document.createElement("script");
+    script.src = "https://www.google.com/jsapi";
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+    return this;
+}());
--- root/assets/lib/decorator/site/head.class.php
+++ root/assets/lib/decorator/site/head.class.php
@@ -43,10 +43,8 @@ class Head_Site_Decorator extends Tag_HTML_Decorator
         
         $this->add_inner_tag_front('meta', false, 
             array('name'=>'viewport', 'content'=>'width=device-width,initial-scale=1,maximum-scale=1'));
-        $this->add_inner_tag('script', null, array('src'=>'https://www.google.com/jsapi'));
         $this->add_inner_tag('script', null, array('async'=>'', 'src'=>$handler_js));
         $this->add_inner_tag('script', null, array('async'=>'', 'src'=>'//www.google-analytics.com/ga.js'));
-        $this->add_inner_tag('script', ';if (typeof google!=="undefined") google.load("feeds","1",{nocss:true});');
 
         $this->add_inner_tag('link', false, 
             array('rel'=>'apple-touch-icon-precomposed', 

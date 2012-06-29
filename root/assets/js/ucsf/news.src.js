0c3bf1c1a184715de08a857c8456ee9cb07f01a8
Rich Trott <rtrott@gmail.com>
2012-06-29 11:02:19 -0700

    Offline news handling

--- root/assets/js/ucsf/news.src.js
+++ root/assets/js/ucsf/news.src.js
@@ -66,31 +66,27 @@ ucsf.news = (function () {
             } else {
                 content = ucsf.news.loadFromStorage(storageId);
             }
-            container.innerHTML = template.render(content);
+            container.innerHTML = options.template.render(content);
         });
     };
 
     this.headlines = function (container, storageId, feedUrl, options) {
         "use strict";
-
-        var template;
             
         options = options || {};
 
-        if (options.template) {
-            template = options.template;
-        } else {
-            template =  new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");if(_.s(_.f("feed",c,p,1),c,p,0,9,313,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<div class=\"menu detailed\"><h2>");_.b(_.v(_.f("title",c,p,0)));_.b("</h2><ol>");if(_.s(_.f("entries",c,p,1),c,p,0,70,290,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("  <li>    <a class=\"no-ext-ind\" rel=\"external\" href=\"");_.b(_.v(_.f("link",c,p,0)));_.b("\"><span class=\"external\">");_.b(_.v(_.f("title",c,p,0)));_.b("</span>    <div class=\"smallprint light\">");_.b(_.v(_.d("dateTime.date",c,p,0)));_.b("</div>    <div class=\"smallprint light\">");_.b(_.v(_.d("dateTime.time",c,p,0)));_.b("</div></a>");});c.pop();}_.b("</ol></div>");});c.pop();}if(!_.s(_.f("feed",c,p,1),c,p,1,0,0,"")){_.b("<div class=\"content\"><p>News feed could not be loaded.</p></div>");};return _.fl();;});
+        if (! options.template) {
+            options.template =  new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");if(_.s(_.f("feed",c,p,1),c,p,0,9,313,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("<div class=\"menu detailed\"><h2>");_.b(_.v(_.f("title",c,p,0)));_.b("</h2><ol>");if(_.s(_.f("entries",c,p,1),c,p,0,70,290,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("  <li>    <a class=\"no-ext-ind\" rel=\"external\" href=\"");_.b(_.v(_.f("link",c,p,0)));_.b("\"><span class=\"external\">");_.b(_.v(_.f("title",c,p,0)));_.b("</span>    <div class=\"smallprint light\">");_.b(_.v(_.d("dateTime.date",c,p,0)));_.b("</div>    <div class=\"smallprint light\">");_.b(_.v(_.d("dateTime.time",c,p,0)));_.b("</div></a>");});c.pop();}_.b("</ol></div>");});c.pop();}if(!_.s(_.f("feed",c,p,1),c,p,1,0,0,"")){_.b("<div class=\"content\"><p>News feed could not be loaded.</p></div>");};return _.fl();;});
         }
         
-        if (typeof google !== "undefined") {
+        if ((typeof google !== "undefined") && (navigator.onLine)) {
             if (typeof google.feeds == "undefined") {
                 google.load("feeds","1",{nocss:true, callback:function () { ucsf.news.render(container, storageId, feedUrl, options);}});
             } else {
                 this.render(container, storageId, feedUrl, options);
             }
         } else {
-            container.innerHTML = template.render(ucsf.news.loadFromStorage(storageId));
+            container.innerHTML = options.template.render(ucsf.news.loadFromStorage(storageId));
         }
     };
 

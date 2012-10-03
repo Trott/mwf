9736e3b1d783a5962246ad4902790d916e960545
Rich Trott <rtrott@gmail.com>
2012-10-03 10:13:09 -0700

    rm img.php usage

--- root/research/js/profile.src.js
+++ root/research/js/profile.src.js
@@ -37,7 +37,7 @@ ucsf.ctsiProfile = {
 
                     if (myProfile.hasOwnProperty("PhotoURL")) {
                         profilePhoto = document.createElement("img");
-                        profilePhoto.setAttribute("src", "/assets/min/img.php?max_height=80&img=" + myProfile.PhotoURL);
+                        profilePhoto.setAttribute("src", "http://src.sencha.io/80/" + myProfile.PhotoURL);
                         profilePhoto.setAttribute("alt", "");
                         profilePhoto.setAttribute("style", "border-top-right-radius:0;border-bottom-left-radius:.5em;float:left");
                         headerHeight = header.clientHeight;

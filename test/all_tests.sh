7f4b331808efd437d477ed69406dbee8479679d4
Rich Trott <rtrott@gmail.com>
2012-12-01 19:07:59 -0800

    Update tests for Firefox

--- test/all_tests.sh
+++ test/all_tests.sh
@@ -5,8 +5,5 @@ phpunit -c php_unit/phpunit.xml
 # test in iPhone simulator
 cucumber
 
-# test in Safari
-for feature in `ls features/*.feature`
-do
-  cucumber BROWSER=firefox $feature
-done
+# test in Firefox
+cucumber BROWSER=firefox

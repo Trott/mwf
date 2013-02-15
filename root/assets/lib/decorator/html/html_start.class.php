24340a8a311d7720c1769b0c577c90c85a1bc2d9
Rich Trott <rtrott@gmail.com>
2013-02-15 10:19:43 -0800

    rm Config object from HTML Start class

--- root/assets/lib/decorator/html/html_start.class.php
+++ root/assets/lib/decorator/html/html_start.class.php
@@ -11,26 +11,18 @@
  * @license http://mwf.ucla.edu/license
  * @version 20111027
  *
- * @uses Config
  * @uses Tag_Open_HTML_Decorator
  */
-require_once(dirname(dirname(__DIR__)) .'/config.class.php');
 require_once(__DIR__ . '/tag_open.class.php');
 
 class HTML_Start_HTML_Decorator extends Tag_Open_HTML_Decorator {
 
     public function __construct($params = array()) {
-        if (!array_key_exists('lang', $params)) {
-            $lang = Config::get('global', 'language');
-            if ($lang != false) {
-                $params['lang'] = $lang;
-            }
-        }
         parent::__construct('html', $params);
     }
     
     public function add_appcache() {
-        return parent::set_param('manifest',Config::get('global','site_assets_url').'/appcache.php');
+        return parent::set_param('manifest','/assets/appcache.php');
     }
 
     public function render($raw = false) {
